12b5b95ae480081c2fe07f7a80866709020562ac
ale-simoes12 <alessandro.medrano321@gmail.com>
2025-01-16 09:10:39 -0300

    extraindo tweets dos ultimos 6 dias

--- airflow_pipeline/dags/x_dag.py
+++ airflow_pipeline/dags/x_dag.py
@@ -6,16 +6,23 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 from os.path import join
 from operators.twitter_operator import TwitterOperator
+from airflow.utils.dates import days_ago
 
-with DAG(dag_id = "TwitterTest", start_date=datetime.now()) as dag:
+
+with DAG(dag_id = "TwitterDag", start_date=days_ago(6), schedule_interval="@daily") as dag:
     
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"    
-    end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
-    start_time = (datetime.now() +  timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)   
+
+
+
+
     query = "datascience"
     to = TwitterOperator(file_path=join("datalake/twitter_datascience",
-                            "extract_date={{ds}}",
-                            "datascience_{{ds_nodash}}.json"),query=query, start_time=start_time , end_time=end_time, task_id="test_run")
+                        "extract_date={{ds}}",
+                        "datascience_{{ds_nodash}}.json"),query=query,
+                        start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
+                        end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
+                        task_id="twitter_datascience")
 
 
 
